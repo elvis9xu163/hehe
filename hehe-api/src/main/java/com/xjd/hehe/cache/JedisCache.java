e864ce13b265cfae908f30b7b559712a79fb7a49
XJD <jdxu@91jkys.com>
2015-11-25 16:37:41 +0800

    绕开Jedis的Broken pipe

--- hehe-api/src/main/java/com/xjd/hehe/cache/JedisCache.java
+++ hehe-api/src/main/java/com/xjd/hehe/cache/JedisCache.java
@@ -2,14 +2,18 @@ package com.xjd.hehe.cache;
 
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.net.SocketException;
 import java.nio.charset.Charset;
+
+import org.perf4j.aop.Profiled;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cache.Cache;
+import org.springframework.cache.support.SimpleValueWrapper;
 
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
-
-import org.perf4j.aop.Profiled;
-import org.springframework.cache.Cache;
-import org.springframework.cache.support.SimpleValueWrapper;
+import redis.clients.jedis.exceptions.JedisConnectionException;
 
 import com.esotericsoftware.kryo.io.Input;
 import com.esotericsoftware.kryo.io.Output;
@@ -19,6 +23,8 @@ import com.esotericsoftware.kryo.io.Output;
  * @since 2015-11-15 09:41
  */
 public class JedisCache implements Cache {
+	private static Logger log = LoggerFactory.getLogger(JedisCache.class);
+
 	protected Charset defaultCharset = Charset.forName("UTF-8");
 	protected String name;
 	protected KryoSerializer kryoSerializer;
@@ -78,31 +84,52 @@ public class JedisCache implements Cache {
 
 	@Profiled
 	protected Object getObject(Object key) {
-		try (Jedis jedis = jedisPool.getResource()) {
-			byte[] bytes = jedis.get(key.toString().getBytes(defaultCharset));
-			if (bytes == null) {
-				return null;
+		while (true) {
+			try (Jedis jedis = jedisPool.getResource()) {
+				byte[] bytes = jedis.get(key.toString().getBytes(defaultCharset));
+				if (bytes == null) {
+					return null;
+				}
+				Object val = kryoSerializer.getKryo().readClassAndObject(new Input(new ByteArrayInputStream(bytes)));
+				return val;
+			} catch (JedisConnectionException e) {
+				if (e.getCause() != null && e.getCause() instanceof SocketException
+						&& e.getCause().getMessage().indexOf("Broken pipe") != -1) {
+					log.warn("jedis broken pipe occured.");
+					continue;
+				} else {
+					throw e;
+				}
 			}
-			Object val = kryoSerializer.getKryo().readClassAndObject(new Input(new ByteArrayInputStream(bytes)));
-			return val;
 		}
 	}
 
 	@Override
 	@Profiled
 	public void put(Object o, Object o1) {
-		try (Jedis jedis = jedisPool.getResource()) {
-			if (o1 == null) {
-				jedis.del(o.toString().getBytes(defaultCharset));
-			} else {
-				ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
-				Output out = new Output(outputStream);
-				kryoSerializer.getKryo().writeClassAndObject(out, o1);
-				out.close();;
-				byte[] bytes = outputStream.toByteArray();
-				byte[] key = o.toString().getBytes(defaultCharset);
-				jedis.set(key, bytes);
-				jedis.expire(key, expireSeconds);
+		while (true) {
+			try (Jedis jedis = jedisPool.getResource()) {
+				if (o1 == null) {
+					jedis.del(o.toString().getBytes(defaultCharset));
+				} else {
+					ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+					Output out = new Output(outputStream);
+					kryoSerializer.getKryo().writeClassAndObject(out, o1);
+					out.close();
+					;
+					byte[] bytes = outputStream.toByteArray();
+					byte[] key = o.toString().getBytes(defaultCharset);
+					jedis.set(key, bytes);
+					jedis.expire(key, expireSeconds);
+				}
+			} catch (JedisConnectionException e) {
+				if (e.getCause() != null && e.getCause() instanceof SocketException
+						&& e.getCause().getMessage().indexOf("Broken pipe") != -1) {
+					log.warn("jedis broken pipe occured.");
+					continue;
+				} else {
+					throw e;
+				}
 			}
 		}
 	}
